728e98d00778c57e8cee4c8afa2b273d4799d8b0
Lars de Kooter <de.kooter.lars@gmail.com>
2024-12-27 18:06:55 +0100

    fix rendering issue

--- components/MeetTable.tsx
+++ components/MeetTable.tsx
@@ -64,7 +64,7 @@ export default function MeetsTable({
                   },
                 ]}
               >
-                {currentItem.data.name}
+                {currentItem.data?.name}
               </Text>
               <View
                 style={{
