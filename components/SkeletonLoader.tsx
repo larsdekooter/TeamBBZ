18d7bd1d6c0e0e5b73a0e04c4467a2d1760574f1
Lars de Kooter <de.kooter.lars@gmail.com>
2025-08-13 21:00:44 +0200

    Improve skeleton loader

--- components/SkeletonLoader.tsx
+++ components/SkeletonLoader.tsx
@@ -1,5 +1,6 @@
+import { transform } from "@babel/core";
 import { useEffect, useState } from "react";
-import { Dimensions, Text } from "react-native";
+import { Dimensions, Text, View, StyleSheet } from "react-native";
 import Animated, {
   useAnimatedStyle,
   useSharedValue,
@@ -7,47 +8,65 @@ import Animated, {
   withSequence,
   withTiming,
 } from "react-native-reanimated";
+import { LinearGradient } from "expo-linear-gradient";
 
 const { width } = Dimensions.get("window");
 const duration = 1200;
 
+const screenWidth = Dimensions.get("window").width;
+
 export default function SkeletonLoader() {
-  const alpha = useSharedValue(0.3);
+  const translateX = useSharedValue(-screenWidth);
   const [height, setHeight] = useState(0);
 
   useEffect(() => {
-    alpha.value = withRepeat(
-      withSequence(
-        withTiming(0.7, { duration }),
-        withTiming(0.3, { duration })
-      ),
+    translateX.value = withRepeat(
+      withTiming(screenWidth, { duration: 2400 }),
       -1,
-      true
+      false
     );
-  });
+  }, []);
 
-  const backgroundAnimationStyle = useAnimatedStyle(() => ({
-    backgroundColor: `rgba(200,200,200,${alpha.value})`,
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ translateX: translateX.value }],
   }));
 
   return (
-    <Animated.View
-      style={[
-        {
-          height,
-          width: width * 0.95,
-          borderRadius: 6,
-          marginVertical: 10,
-        },
-        backgroundAnimationStyle,
-      ]}
+    <View
+      style={{
+        backgroundColor: "rgb(17, 17, 17)",
+        overflow: "hidden",
+        height,
+        width: screenWidth * 0.95,
+        borderRadius: 6,
+        marginVertical: 10,
+      }}
     >
-      <Text
-        style={{ height: 0 }}
-        onTextLayout={(e) => setHeight(e.nativeEvent.lines[0].height + 20)}
+      <Animated.View
+        style={[StyleSheet.absoluteFill, animatedStyle, { borderRadius: 6 }]}
       >
-        dummy text
-      </Text>
-    </Animated.View>
+        <LinearGradient
+          colors={[
+            "rgb(17, 17, 17)",
+            "rgb(51, 51, 51)",
+            "rgb(51, 51, 51)",
+            "rgb(17, 17, 17)",
+            "rgb(17, 17, 17)",
+            "rgb(17, 17, 17)",
+            "rgb(17, 17, 17)",
+            "rgb(17, 17, 17)",
+          ]}
+          start={{ x: 0, y: 0 }}
+          end={{ x: 2, y: 0 }}
+          style={StyleSheet.absoluteFill}
+        />
+        <Text
+          style={{ height: 0 }}
+          onTextLayout={(e) => setHeight(e.nativeEvent.lines[0].height + 20)}
+        >
+          dummy text
+        </Text>
+      </Animated.View>
+    </View>
   );
 }
