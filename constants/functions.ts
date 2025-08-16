c7efff711978aa30adc0b2cc81c4b35ab15c8d58
larsdekooter <102305474+larsdekooter@users.noreply.github.com>
2025-08-16 20:34:42 +0200

    Remove explicit date in schema fetching

--- constants/functions.ts
+++ constants/functions.ts
@@ -340,7 +340,7 @@ export async function getWedstrijdData(wedstrijd: Wedstrijd) {
 
 export async function getSchemaData(group?: string) {
   const date = new Date();
-  const weekNo = getWeekNumber(new Date(date.setDate(11)));
+  const weekNo = getWeekNumber(date);
 
   const res = await (
     await fetch(
