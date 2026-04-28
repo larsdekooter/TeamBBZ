1b2206aa8e573f3885ff56c30eeba23f27fb2571
Lars de Kooter <de.kooter.lars@gmail.com>
2026-04-28 18:28:07 +0200

    remove M from time

--- Swimrankingsexport.js
+++ Swimrankingsexport.js
@@ -63,7 +63,7 @@ function formatEvent(event) {
 for (const row of rows) {
   const timeObject = {
     event: formatEvent(row.children[0].children[0].innerText),
-    time: row.children[2].children[0].innerText.replace(/m/g, ""),
+    time: row.children[2].children[0].innerText.replace(/m|M/g, ""),
     poolSize: row.children[1].innerText,
     points: parseInt(row.children[3].innerText),
     swimmer,
